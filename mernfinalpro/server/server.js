415bd30cad5ea9ac45f68a47cd70e86c9068b0f9
Sithandekile Ndhlovu <152465186+sithandekile@users.noreply.github.com>
2026-09-05 02:23:50 +0200

    Remove dotenv import from server.js
    Removed dotenv configuration import from server.js

--- mernfinalpro/server/server.js
+++ mernfinalpro/server/server.js
@@ -8,7 +8,6 @@ const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const compression = require("compression");
 const connectDB=require('./config/db')
-require("dotenv").config();
 
 // // Import routes
 const productRoutes = require("./routes/productRoutes");
